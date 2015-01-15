18fbf20d5c52dce9fc68987937a3aa2c1869cb69
Edouard Oger <edouard.oger@gmail.com>
2015-01-15 16:43:19 +0100

    Tests cleanup

--- test/handlers/documents.js
+++ test/handlers/documents.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = require('../app.js');
+var app = require('../../app.js');
 var should = require('should');
 var request = require('supertest');
 
@@ -21,66 +21,49 @@ describe('<Documents endpoint>', function() {
         .get('/documents')
         .query({ current_provider_id: '123', search: 'test document', strict: 'maybe' })
         .set('Accept', 'application/json')
-        .end(function(err, res) {
-          should.exist(err);
-          res.should.have.status(400);
-          done();
-        });
+        .expect(400)
+        .end(done);
     });
-  });
 
-  describe('GET /documents', function() {
     it("limit is negative", function(done) {
       request(app)
         .get('/documents')
         .query({ current_provider_id: '123', search: 'test document', limit: '-1' })
         .set('Accept', 'application/json')
-        .end(function(err, res) {
-          should.exist(err);
-          res.should.have.status(400);
-          done();
-        });
+        .expect(400)
+        .end(done);
     });
-  });
 
-  describe('GET /documents', function() {
     it("start is negative", function(done) {
       request(app)
         .get('/documents')
         .query({ current_provider_id: '123', search: 'test document', start: '-1' })
         .set('Accept', 'application/json')
-        .end(function(err, res) {
-          should.exist(err);
-          res.should.have.status(400);
-          done();
-        });
+        .expect(400)
+        .end(done);
     });
-  });
 
-  describe('GET /documents', function() {
     it("should not find anything", function(done) {
       request(app)
         .get('/documents')
         .query({ current_provider_id: '123xxx', search: 'waldo' })
         .set('Accept', 'application/json')
+        .expect(200)
         .end(function(err, res) {
           should.not.exist(err);
-          res.should.have.status(200);
           res.body.should.be.empty;
           done();
         });
     });
-  });
 
-  describe('GET /documents', function() {
     it("should find some documents", function(done) {
       request(app)
         .get('/documents')
         .query({ current_provider_id: '123xxx', search: 'report' })
         .set('Accept', 'application/json')
+        .expect(200)
         .end(function(err, res) {
           should.not.exist(err);
-          res.should.have.status(200);
           res.body.should.have.length(3);
           res.body[0].id.should.be("1234769");
           res.body[1].id.should.be("zzzzzzz");
@@ -88,68 +71,59 @@ describe('<Documents endpoint>', function() {
           done();
         });
     });
-  });
 
-
-  describe('GET /documents', function() {
     it("should find less documents (strict)", function(done) {
       request(app)
         .get('/documents')
         .query({ current_provider_id: '123xxx', search: 'report', strict: 'true' })
         .set('Accept', 'application/json')
+        .expect(200)
         .end(function(err, res) {
           should.not.exist(err);
-          res.should.have.status(200);
           res.body.should.have.length(3);
           res.body[0].id.should.be("1234769");
           res.body[1].id.should.be("zzzzzzz");
           done();
         });
     });
-  });
 
-  describe('GET /documents', function() {
     it("should find less documents (start param)", function(done) {
       request(app)
         .get('/documents')
         .query({ current_provider_id: '123xxx', search: 'report', start: '1' })
         .set('Accept', 'application/json')
+        .expect(200)
         .end(function(err, res) {
           should.not.exist(err);
-          res.should.have.status(200);
           res.body.should.have.length(2);
           res.body[0].id.should.be("zzzzzzz");
           res.body[1].id.should.be("aaaaaaa");
           done();
         });
     });
-  });
 
-  describe('GET /documents', function() {
     it("should find even less documents (limit param)", function(done) {
       request(app)
         .get('/documents')
         .query({ current_provider_id: '123xxx', search: 'report', limit: '1' })
         .set('Accept', 'application/json')
+        .expect(200)
         .end(function(err, res) {
           should.not.exist(err);
-          res.should.have.status(200);
           res.body.should.have.length(1);
           res.body[0].id.should.be("1234769");
           done();
         });
     });
-  });
 
-  describe('GET /documents', function() {
     it("should find even less documents (start+limit param)", function(done) {
       request(app)
         .get('/documents')
         .query({ current_provider_id: '123xxx', search: 'report', start: '1', limit: '1' })
         .set('Accept', 'application/json')
+        .expect(200)
         .end(function(err, res) {
           should.not.exist(err);
-          res.should.have.status(200);
           res.body.should.have.length(1);
           res.body[0].id.should.be("zzzzzzz");
           done();
