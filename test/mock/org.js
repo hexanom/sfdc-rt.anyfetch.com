399fd7ec43a8e79637419ef3b794191b0928188b
Edouard Oger <edouard.oger@gmail.com>
2015-01-19 09:41:35 +0100

    first draft at mocking sfdc

--- test/mock/org.js
+++ test/mock/org.js
@@ -1,6 +1,10 @@
 "use strict";
 
+var Record = require('./record');
+var SearchResult = require('./searchresult');
+
 var mOauth = "mockOauth";
+
 
 module.exports = {
   authenticate: function(_, cb) {
@@ -10,13 +14,13 @@ module.exports = {
     if(opts.oauth === mOauth) {
       switch(opts.id) {
         case "test":
-          cb(null, {});
+          cb(null, new Record("12/12/2014", "13/12/2014", "record #1"));
           break;
 
       }
     }
   },
   search: function(opts, cb) {
-    cb(null, null);
+    cb(null, [new SearchResult(1), new SearchResult(2)]);
   }
 };
---
+++ test/mock/searchresult.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var SearchResult = function(id) {
+  this.id = id;
+};
+
+SearchResult.prototype.getId = function() {
+  return this.id;
+};
