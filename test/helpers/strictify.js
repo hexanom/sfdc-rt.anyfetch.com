6fb5c97c981c442b1b08ae53c259c83474a66bf4
Robin Ricard <ricard.robin@gmail.com>
2015-01-21 09:31:04 +0100

    Strictify tests for quotes and ANDs

--- test/helpers/strictify.js
+++ test/helpers/strictify.js
@@ -1,4 +1,19 @@
 "use strict";
 
+var strictify = require('../../lib').helpers.strictify;
+
 describe("<Strictify helper>", function() {
+  it("should replace spaces by ANDs", function() {
+    strictify("Jack Bob")
+      .should.be.exactly("Jack AND Bob");
+    strictify("Jack Bob Bobby")
+      .should.be.exactly("Jack AND Bob AND Bobby");
+  });
+
+  it("should ignore quotes", function() {
+    strictify('"Jack Bob"')
+      .should.be.exactly('"Jack Bob"');
+    strictify('"Jack Bob" Bobby')
+      .should.be.exactly('"Jack Bob" AND Bobby');
+  });
 });
